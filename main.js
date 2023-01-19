6587d5f849108afc876453a5b02fbfc0b15b7916
ZoiloMendoza <zoilomm@hotmail.com>
2023-01-18 18:36:23 -0600

    añadiendo try/catch

--- main.js
+++ main.js
@@ -19,17 +19,21 @@ fetch(url)
     });
     */
 const mainOption2 = async() => {
-    const url = 'https://rickandmortyapi.com/api/character';
-    const response = await fetch(url);
-    const body = await response.json();
-    body.results.forEach((personaje) => {
-        const contenedorImagen = `
-        <div class="col p-1">
-            <img src="${personaje.image}">
-            <p>${personaje.name}</p>
-        </div>`;
-        imagenes.insertAdjacentHTML('beforeend',contenedorImagen);
-    });
+    try {
+        const url = 'https://rickandmortyapi.com/api/character';
+        const response = await fetch(url);
+        const body = await response.json();
+        body.results.forEach((personaje) => {
+            const contenedorImagen = `
+            <div class="col p-1">
+              <img src="${personaje.image}">
+              <p>${personaje.name}</p>
+            </div>`;
+            imagenes.insertAdjacentHTML('beforeend',contenedorImagen);
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 mainOption2();
