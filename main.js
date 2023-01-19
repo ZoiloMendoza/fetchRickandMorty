29de45300a280a6a3024d6de37a4a7d18958f4f4
ZoiloMendoza <zoilomm@hotmail.com>
2023-01-18 18:29:12 -0600

    peticion get usando async/await

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const imagenes = document.getElementById('imagenes');
 
+/*
 const url = 'https://rickandmortyapi.com/api/character';
 fetch(url)
     .then((response) =>response.json())
@@ -16,3 +17,19 @@ fetch(url)
     .catch((error) =>{
         console.log(error);
     });
+    */
+const mainOption2 = async() => {
+    const url = 'https://rickandmortyapi.com/api/character';
+    const response = await fetch(url);
+    const body = await response.json();
+    body.results.forEach((personaje) => {
+        const contenedorImagen = `
+        <div class="col p-1">
+            <img src="${personaje.image}">
+            <p>${personaje.name}</p>
+        </div>`;
+        imagenes.insertAdjacentHTML('beforeend',contenedorImagen);
+    });
+}
+
+mainOption2();
