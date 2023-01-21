52e99f42ec2ee15a59407e78ca98bb53705b2398
ZoiloMendoza <zoilomm@hotmail.com>
2023-01-20 18:45:28 -0600

    agregando funcionalidad personaje

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const imagenes = document.getElementById('imagenes');
 
-/*
+/*mainOption1
 const url = 'https://rickandmortyapi.com/api/character';
 fetch(url)
     .then((response) =>response.json())
@@ -17,7 +17,7 @@ fetch(url)
     .catch((error) =>{
         console.log(error);
     });
-    */
+*/
 const mainOption2 = async() => {
     try {
         const url = 'https://rickandmortyapi.com/api/character';
@@ -26,7 +26,7 @@ const mainOption2 = async() => {
         body.results.forEach((personaje) => {
             const contenedorImagen = `
             <div class="col p-1">
-              <img src="${personaje.image}">
+              <img src="${personaje.image}" id="${personaje.id}">
               <p>${personaje.name}</p>
             </div>`;
             imagenes.insertAdjacentHTML('beforeend',contenedorImagen);
@@ -36,4 +36,26 @@ const mainOption2 = async() => {
     }
 }
 
+
+imagenes.addEventListener('click', (event) => {
+   
+    let url = `https://rickandmortyapi.com/api/character/${event.target.id}`
+    fetch(url)
+    .then( resp => resp.json())
+    .then( personaje => {
+        let body = document.querySelector('body');
+        const contenedorImagen = `
+            <div class="col p-1">
+              <img src="${personaje.image}">
+              <p>${personaje.name}</p>
+            </div>`;
+            body.innerHTML = contenedorImagen;
+    })
+    .catch( error => {
+        console.log(error);
+    })
+})
+
 mainOption2();
+
+
